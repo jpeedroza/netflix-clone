2ea83ee82d7bfb6824f809c865a2b28ed94dcb45
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-02-16 13:29:35 -0300

    [Update] New component to landing page

---
+++ src/components/Products.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import tv from "../assets/img/tv.png";
+import mobile from "../assets/img/mobile-0819.jpg";
+import devices from "../assets/img/device-pile.png";
+
+function Products() {
+  return (
+    <div>
+      <ul className="bg-black">
+        <li className="flex flex-row items-center justify-between px-52 border-b-8 border-gray-600">
+          <div className="mx-24">
+            <h1 className="text-7xl font-medium text-white mb-3">
+              Enjoy on your TV.
+            </h1>
+            <h2 className="text-3xl font-normal text-white mt-4">
+              Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV,
+              Blu-ray players, and more.
+            </h2>
+          </div>
+          <img src={tv} alt="Teve" />
+        </li>
+        <li className="flex flex-row-reverse items-center justify-between px-52 border-b-8 border-gray-600">
+          <div className="mx-10">
+            <h1 className="text-7xl font-medium text-white mb-3">
+              Download your shows to watch offline.
+            </h1>
+            <h2 className="text-3xl font-normal text-white mt-4">
+              Save your favorites easily and always have something to watch.
+            </h2>
+          </div>
+          <img src={mobile} alt="Celular" />
+        </li>
+        <li className="flex flex-row items-center justify-between px-52 border-b-8 border-gray-600">
+          <div className="mx-16">
+            <h1 className="text-7xl font-medium text-white mb-3">
+              Watch everywhere.
+            </h1>
+            <h2 className="text-3xl font-normal text-white mt-4">
+              Stream unlimited movies and TV shows on your phone, tablet,
+              laptop, and TV without paying more.
+            </h2>
+          </div>
+          <img src={devices} alt="Aparelhos" />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Products.propTypes = {};
+
+export default Products;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "./assets/img/logo.png";
 import "./assets/css/index.css";
 import HeadingPage from "./components/HeadingPage";
+import Products from "./components/Products";
 
 function App() {
   return (
     <>
       <HeadingPage logo={logo} />
+      <Products />
     </>
   );
 }
